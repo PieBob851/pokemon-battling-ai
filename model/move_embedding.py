2a42682530c48c6a2027e3011c22996bee0755fa
ljones315 <ltjones2002@gmail.com>
2024-11-30 23:42:53 -0500

    embed moves stats

--- model/move_embedding.py
+++ model/move_embedding.py
@@ -1,27 +1,92 @@
 import torch
 import torch.nn as nn
 import pandas as pd
+import json
 
-# List of moves retrieved from https://bulbapedia.bulbagarden.net/wiki/List_of_moves
-moves_df = pd.read_csv('data/moves.csv')
-# num represents move ID
-MOVE_NAME_TO_NUM = dict(zip(moves_df['name'], moves_df['num']))
+with open('data/moves.json', 'r') as file:
+    moves_data = json.load(file)
 
-# Note that there are 901 total moves in MOVE_NAME_TO_NUM dictionary. However, the identifers 'num' do NOT
-# range from 0 to 900 as some are missing. Therefore, vocab_size will be slightly higher than actual number
-# of possible moves to allow lookup without out of index error in the Embedding.
-VOCAB_SIZE = max(MOVE_NAME_TO_NUM.values()) + 1
+MOVE_NAME_TO_ATTRIBUTES = {
+    move["name"].lower().replace(' ', '').replace('-', ''): {
+        "basePower": move.get("basePower", 0),
+        "accuracy": 1 if move.get("accuracy", True) == True else move.get("accuracy", 0),
+        "type": move.get("type", "None"),
+        "category": move.get("category", "None"),
+        "pp": move.get("pp", 0)
+    }
+    for move in moves_data
+}
+
+TYPE_TO_INDEX = {
+    "Normal": 0,
+    "Fire": 1,
+    "Water": 2,
+    "Electric": 3,
+    "Grass": 4,
+    "Ice": 5,
+    "Fighting": 6,
+    "Poison": 7,
+    "Ground": 8,
+    "Flying": 9,
+    "Psychic": 10,
+    "Bug": 11,
+    "Rock": 12,
+    "Ghost": 13,
+    "Dragon": 14,
+    "Dark": 15,
+    "Steel": 16,
+    "Fairy": 17,
+    "None": 18
+}
+
+CATEGORY_TO_INDEX = {
+    "Physical": 0,
+    "Special": 1,
+    "Status": 2,
+    "None": 3
+}
+
+NUMERICAL_INPUT_SIZE = 3
+
 
 class MoveEmbedding(nn.Module):
-    def __init__(self, embedding_dim, vocab_size=VOCAB_SIZE):
+    def __init__(self, embedding_dim,
+                 numerical_input_size=NUMERICAL_INPUT_SIZE,
+                 type_vocab_size=len(TYPE_TO_INDEX),
+                 category_vocab_size=len(CATEGORY_TO_INDEX), ):
         super().__init__()
-        self.move_embed = nn.Embedding(vocab_size, embedding_dim)
+        self.type_embed = nn.Embedding(type_vocab_size, embedding_dim)
+        self.category_embed = nn.Embedding(category_vocab_size, embedding_dim)
+
+        self.numerical_linear = nn.Linear(numerical_input_size, embedding_dim)
+        self.combine_fc = nn.Linear(embedding_dim * 3, embedding_dim)
 
     def forward(self, move_name):
-        # if move_name does NOT exist, default to 0 since move numbering starts from 1.
-        move_num = MOVE_NAME_TO_NUM.get(move_name, 0)
-        move_embed = self.move_embed(torch.tensor(move_num))
-        return move_embed
+        move_info = MOVE_NAME_TO_ATTRIBUTES.get(move_name, {
+            "basePower": 0,
+            "accuracy": 0,
+            "type": "None",
+            "category": "None",
+            "pp": 0
+        })
+        # print(move_info)
+        type_embed = self.type_embed(torch.tensor(TYPE_TO_INDEX.get(move_info["type"])))
+        category_embed = self.category_embed(torch.tensor(CATEGORY_TO_INDEX.get(move_info["category"])))
+
+        numerical_inputs = self.get_numerical_inputs(move_info)
+        numerical_embed = self.numerical_linear(numerical_inputs)
+
+        final_embed = torch.cat((type_embed, category_embed, numerical_embed), dim=-1)
+        return self.combine_fc(final_embed)
+
+    def get_numerical_inputs(self, move_info):
+        accuracy = move_info["accuracy"]
+        if (accuracy is True):
+            accuracy = 100
+        basePower = move_info["basePower"]
+        pp = move_info["pp"]
+        numerical_inputs = torch.tensor([accuracy, basePower, pp]).float()
+        return numerical_inputs
 
 
 """
